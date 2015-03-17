3c1390836a34abd3681311dd93858d6ad348ca4f
AlexChen12 <xfchen12@yeah.net>
2015-03-17 13:32:51 +0800

    add test project

---
+++ Test/Test.py
@@ -0,0 +1,13 @@
+# Script Name: Test.py
+# Author     : AlexChen
+# Last Modified: 
+# Version    : 1.0
+
+import sys
+import os
+
+# for test
+
+def readfile(filename):
+	f=open(filename,'r')
+	line
